c6527824052efe3b9b63a4149330d483e0f6a411
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-07-14 22:34:21 -0400

    feat: agregar configuración de router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const HomeView = () => import('@views/HomeView.vue')
+const RutaView = () => import('@views/RutaView.vue')
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/ruta/:id',
+    name: 'ruta',
+    component: RutaView,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/',
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) return savedPosition
+    return { top: 0 }
+  },
 })
 
 export default router
