c70701c1e20f14ee0eae1e5724198a7c0a75d3ab
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-07-14 22:35:28 -0400

    feat: registrar plugins

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import i18n from './i18n'
+
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
+import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(i18n)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: 'system',
+    },
+  },
+})
 
 app.mount('#app')
