86a6497a393dc4c0e21252d288b2953d46cc011d
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2026-09-10 23:00:04 -0300

    agregar imagen y color de ruta

--- src/views/RutaView.vue
+++ src/views/RutaView.vue
@@ -4,23 +4,38 @@ import { useRoute } from 'vue-router'
 /* import { useI18n } from 'vue-i18n' */
 import { getRutaById } from '@/utils/rutaHelpers'
 import MapaInteractivo from '@/components/ruta/MapaInteractivo.vue'
+import AppNavbar from '@/components/layout/AppNavbar.vue'
 
 /* const { t } = useI18n() */
 const route = useRoute()
 
 const ruta = computed(() => getRutaById(route.params.rutaId))
+/* const getImageUrl = (nombre) => {
+  if (!nombre) return ''
+  return new URL(`../assets/images/${nombre}`, import.meta.url).href
+} */
 </script>
 
 <template>
-  <div class="p-12">
-    <template v-if="ruta">
-      <h1>{{ ruta.nombre }}</h1>
-      <p>{{ ruta.descripcion }}</p>
-      <!-- Se pasan los hitos y el color identificador de la ruta -->
-      <MapaInteractivo :hitos="ruta.hitos" :color-ruta="ruta.color" />
-    </template>
-    <template v-else>
-      <h1>ruta no encontrada</h1>
-    </template>
+  <div>
+    <section class="relative flex flex-col min-h-screen overflow-hidden">
+      <img :src="ruta.imagen_portada" alt="" class="absolute object-cover object-top -z-10">
+      <div :style="{ backgroundColor: ruta.color }" class="mix-blend-multiply absolute inset-0"></div>
+
+      <AppNavbar />
+    </section>
+
+    <div class="p-12">
+      <template v-if="ruta">
+        <h1>{{ ruta.nombre }}</h1>
+
+        <p>{{ ruta.descripcion }}</p>
+        <!-- Se pasan los hitos y el color identificador de la ruta -->
+        <MapaInteractivo :hitos="ruta.hitos" :color-ruta="ruta.color" />
+      </template>
+      <template v-else>
+        <h1>ruta no encontrada</h1>
+      </template>
+    </div>
   </div>
 </template>
